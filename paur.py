0134583db4b7c2b1781336884070624472e8f957
kuche1 <znamsiparolata4esno@abv.bg>
2025-08-21 18:12:22 +0300

    update

--- paur.py
+++ paur.py
@@ -6,12 +6,15 @@ from colorama import Fore, Style
 from datetime import datetime
 import sys
 import tempfile
+import subprocess
+import argparse
 
 COL_PKG_NAME = Fore.CYAN
 COL_PKG_VER = Fore.BLUE
 COL_PKG_VOTES = Fore.GREEN
 COL_PKG_OUTDATED = Fore.RED
 COL_PKG_SELECTION = Fore.MAGENTA
+COL_PKG_INSTALLED = Fore.YELLOW
 
 URL = 'https://aur.archlinux.org/rpc/'
 
@@ -30,12 +33,11 @@ class Package:
     # maintainer: str
     out_of_date: None | int # int - unix timestamp
 
-
 def search_package(name: str) -> list[Package]:
     params = {
         'v': '5', # api version
         'type': 'search',
-        'arg': 'vmware'
+        'arg': name
     }
 
     response = requests.get(URL, params=params)
@@ -51,23 +53,39 @@ def search_package(name: str) -> list[Package]:
 
     return packages
 
-def main() -> None:
-    packages = search_package('vmware')
-    packages.sort(key=lambda pkg: (pkg.votes, pkg.popularity))
+def get_installed_package_version(name: str) -> None | str:
+    proc = subprocess.run(['pacman', '-Q', name], check=False, capture_output=True)
+    if proc.returncode != 0:
+        return None
+    _name, version = proc.stdout.decode().strip().split(' ')
+    return version
+
+def main(package_to_search_for: str) -> None:
+    packages = search_package(package_to_search_for)
+    packages.sort(reverse=True, key=lambda pkg: (pkg.votes, pkg.popularity))
 
     for package_idx, package in reversed(list(enumerate(packages))):
-        print(f'{COL_PKG_SELECTION}{package_idx}{Style.RESET_ALL}/{COL_PKG_NAME}{package.name}{Style.RESET_ALL} {COL_PKG_VER}{package.version}{Style.RESET_ALL} {COL_PKG_VOTES}[+{package.votes} ~{round(package.popularity, 2)}]{Style.RESET_ALL}', end='')
+        print(f'{COL_PKG_SELECTION}{package_idx}{Style.RESET_ALL}/{COL_PKG_NAME}{package.name}{Style.RESET_ALL}', end='')
+
+        installed_version = get_installed_package_version(package.name)
+        if installed_version is not None:
+            print(f' {COL_PKG_INSTALLED}[installed: {installed_version}]{Style.RESET_ALL}', end='')
+
+        print(f' {COL_PKG_VER}{package.version}{Style.RESET_ALL} {COL_PKG_VOTES}[+{package.votes} ~{round(package.popularity, 2)}]{Style.RESET_ALL}', end='')
 
         if package.out_of_date is not None:
             dt = datetime.fromtimestamp(package.out_of_date)
-            print(f' {COL_PKG_OUTDATED}OUT-OF-DATE {dt.strftime("%Y/%m/%d")}{Style.RESET_ALL}')
+            print(f' {COL_PKG_OUTDATED}OUT-OF-DATE {dt.strftime("%Y/%m/%d")}{Style.RESET_ALL}', end='')
             # IMPROVE: compare to the current "system snapshot date"
-        else:
-            print()
+
+        print()
 
         print(f'    {package.desc}')
 
-    choice = input('> ')
+    try:
+        choice = input('> ')
+    except (KeyboardInterrupt, EOFError):
+        sys.exit(0)
 
     try:
         choice = int(choice)
@@ -80,11 +98,10 @@ def main() -> None:
         sys.exit(1)
 
     package = packages[choice]
-
+    print(f'{package=}')
 
     with tempfile.TemporaryDirectory() as tmpdir:
-        term(['git', 'clone', f'https://aur.archlinux.org/{package.name}.git'])
-
+        subprocess.run(['git', 'clone', f'https://aur.archlinux.org/{package.name}.git'], check=True)
 
     # TODO
     # git clone https://aur.archlinux.org/vmware-workstation.git
@@ -99,4 +116,7 @@ if __name__ == '__main__':
     # TODO: add the ability to update all packages
     # TODO: add the ability to install regular `pacman` packages
     # IMPROVE: give the user the ability to use the latest commit instead OR select a commit
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('package', type=str, help='Package to search for')
+    args = parser.parse_args()
+    main(args.package)
